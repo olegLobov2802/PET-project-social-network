302dcfa768ebf7a7b9201cd3a32ff16aa22ee6ca
Oleg <oleg.lobov280214@gmail.com>
2021-03-11 22:17:40 +0300

    append create redux devtools

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -11,4 +11,7 @@ let reducers = combineReducers({
   usersPage: usersReducer,
 });
 
-export const store = createStore(reducers);
+export const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
